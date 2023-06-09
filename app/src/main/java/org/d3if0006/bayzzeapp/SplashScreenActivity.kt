a05c871b294ff3d92ef4be4ccdd21a2712c623a7
AyuSuryaDhemira <dhemirasurya15@gmail.com>
2023-06-09 07:19:48 +0700

    SplasScreen

---
+++ app/src/main/java/org/d3if0006/bayzzeapp/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package org.d3if0006.bayzzeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
